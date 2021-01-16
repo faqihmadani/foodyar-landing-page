12dc36075268d8ee40cedf89adf2ed11faa4c711
faqihmadani <faqihfaqih22@gmail.com>
2021-01-16 16:45:04 +0700

    Nav dan Hero sudah jadi

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily:{
+      'quicksand': 'Quicksand',
+    },
+    colors:{
+      'myBlue': '#272042',
+      'myYellow': '#F7C531',
+      'myGray': '#8B8BA5',
+      'myPink': '#FBE0DC',
+      'white':'#ffffff'
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
